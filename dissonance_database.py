50aada9b45152f144cdd6b1ca24335a9b03b0b7d
egutierrez112999 <egutierrez112999@gmail.com>
2023-02-28 20:11:35 -0700

    Added setup.py

--- dissonance_database.py
+++ dissonance_database.py
@@ -1,6 +1,24 @@
 #This will be our python file for creating queries
 import sqlite3
 
-con = sqlite3.connect("dissonance.db")
-cur = con.cursor()
+class DissDB:
 
+    def __init__(self):
+        self.connection = sqlite3.connect("dissonance.db")
+        self.cursor = self.connection.cursor()
+
+
+    def getAllUsers(self):
+        self.cursor.execute("SELECT * FROM users")
+        return self.cursor.fetchall()
+
+    def getOneUser(self, userID):
+        data = [userID]
+        self.cursor.execute("SELECT * FROM users WHERE user_id = ?", data)
+        return self.cursor.fetchone()
+
+    def createUser(self, user_id, username, email, date_created, password):
+
+db = DissDB()
+print(db.getAllUsers())
+print(db.getOneUser(1000))
---
+++ setup.py
@@ -0,0 +1,4 @@
+import sqlite3
+
+con = sqlite3.connect("dissonance.db")
+cur = con.cursor()
