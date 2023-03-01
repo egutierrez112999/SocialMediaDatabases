d7ca0101123f2cccef2b890bd0e8098423f75d0e
egutierrez112999 <egutierrez112999@gmail.com>
2023-03-01 12:50:04 -0700

    Added commands to get all friends of a user and to creatuser

--- dissonance_database.py
+++ dissonance_database.py
@@ -1,5 +1,6 @@
 #This will be our python file for creating queries
 import sqlite3
+import random
 
 class DissDB:
 
@@ -17,8 +18,27 @@ class DissDB:
         self.cursor.execute("SELECT * FROM users WHERE user_id = ?", data)
         return self.cursor.fetchone()
 
-    def createUser(self, user_id, username, email, date_created, password):
+    def createUser(self, username, email, date_created, password):
+        user_id = random.randint(1005,99999)
+        data = [user_id, username, email, date_created, password]
+        self.cursor.execute("INSERT INTO users (user_id, username, email, date_created, password) VALUES (?,?,?,?,?)", data)
+        self.connection.commit()
+
+    def getFriends(self, user_id):
+        data = [user_id]
+        self.cursor.execute("SELECT users.username, friend_user_id, fu.username FROM users JOIN friends ON users.user_id = friends.user_id JOIN users AS fu ON friends.friend_user_id = fu.user_id WHERE friends.user_id = ?", data)
+        friends = self.cursor.fetchall()
+        return friends
+        
+
+#    Creating users
+#    Adding relationships between users
+#    Posting for a user
+#    Checking the feed for a user (the most recent n posts of people that user follows)
+#    Other interactions to use your additional features
 
 db = DissDB()
+db.createUser('cool_dude', 'cd@dude.com', 'feb_29_2023', 'iamacooldude')
 print(db.getAllUsers())
-print(db.getOneUser(1000))
+#print(db.getOneUser(1000))
+#print(db.getFriends(1000))
