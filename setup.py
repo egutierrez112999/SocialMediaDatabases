4a6b78bb60e7b37e195299405130386463366026
egutierrez112999 <egutierrez112999@gmail.com>
2023-03-01 11:58:06 -0700

    Moving on to adding server relationships

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ cur.execute("DROP TABLE channels")
 cur.execute("DROP TABLE blacklist")
 cur.execute("DROP TABLE owners")
 cur.execute("DROP TABLE friends")
-cur.execute("CREATE TABLE IF NOT EXISTS servers (server_id INTEGER PRIMARY KEY, user_id INTEGER, date_created TEXT)")
+cur.execute("CREATE TABLE IF NOT EXISTS servers (server_id INTEGER, server_name TEXT,user_id INTEGER)")
 cur.execute("CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, username TEXT, email TEXT, date_created TEXT, password TEXT)")
 cur.execute("CREATE TABLE IF NOT EXISTS posts (post_id INTEGER PRIMARY KEY, channel_id INTEGER, server_id INTEGER, user_id INTEGER, message TEXT, timestamp VARCHAR(30))")
 cur.execute("CREATE TABLE IF NOT EXISTS likes (post_id INTEGER PRIMARY KEY, user_id INTEGER, like INTEGER, dislike INTEGER)")
@@ -23,4 +23,24 @@ cur.execute("CREATE TABLE IF NOT EXISTS channels (channel_id INTEGER PRIMARY KEY
 
 #insert Users
 cur.execute("INSERT INTO users (user_id, username, email, date_created, password) VALUES (1000, 'usidorethewizard', 'colby@gmail.com', 'feb_29_2023', 'asdf')")
+cur.execute("INSERT INTO users (user_id, username, email, date_created, password) VALUES (1001, 'sovietbear1945', 'erick@gmail.com', 'feb_29_2023', 'SemiSecurePwd11')")
+cur.execute("INSERT INTO users (user_id, username, email, date_created, password) VALUES (1002, 'fractal13', 'curtis@gmail.com', 'feb_29_2023', 'emacsisawesome')")
+cur.execute("INSERT INTO users (user_id, username, email, date_created, password) VALUES (1003, 'russross', 'russ@gmail.com', 'feb_29_2023', 'apple2easy')")
+cur.execute("INSERT INTO users (user_id, username, email, date_created, password) VALUES (1004, 'bartstander', 'bart@gmail.com', 'feb_29_2023', 'hawaiianstyle')")
+cur.execute("INSERT INTO users (user_id, username, email, date_created, password) VALUES (1005, 'renquinn', 'ren@gmail.com', 'feb_29_2023', 'backonsoylent')")
 con.commit()
+
+#insert servers
+cur.execute("INSERT INTO servers (server_id, server_name, user_id) VALUES (2000, 'cs', 1004)")
+con.commit()
+#insert friends
+
+#insert owners
+
+#insert blacklist
+
+#insert channels
+
+#insert posts
+
+#insert likes
